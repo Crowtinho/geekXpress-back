7b3117331bdfe3e479dd925b3bec88f976f262ca
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-09-22 15:15:35 -0500

    V1.0.7-pages

--- src/main/java/com/geek/back/config/WebConfig.java
+++ src/main/java/com/geek/back/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //.allowedOrigins("https://frontclientes.netlify.app/")
-                .allowedOrigins("http://127.0.0.1:5502")
+                .allowedOrigins("https://crowtinho.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
